347a85451d689dcb77633a0d11e42332efc3495e
rookie <rookielei@gmail.com>
2017-11-20 11:20:17 +0800

    新增 UploadFile.vue

---
+++ my-project/src/components/UploadFile.vue
@@ -0,0 +1,14 @@
+<template>
+  <Upload :action="uploadAction">
+    <Button type="ghost" icon="ios-cloud-upload-outline">Upload files</Button>
+  </Upload>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      'uploadAction': 'server/file-upload'
+    }
+  }
+};
+</script>
--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import UploadFile from '@/components/UploadFile'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/upload',
+      name: 'UploadFile',
+      component: UploadFile
+    },
     {
       path: '/',
       name: 'HelloWorld',
